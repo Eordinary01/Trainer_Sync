8a8af5be230743711c8b976efcc9b7b3be850ae8
Parth <parth.manocha2901@gmail.com>
2026-01-28 10:10:28 +0530

    Implementing a fix for backend routing

--- backend/app.js
+++ backend/app.js
@@ -50,7 +50,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.options('*', cors());
+app.options('/*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
